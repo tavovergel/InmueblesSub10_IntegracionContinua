4e62eede3406ab234fe0ae3b25296990225f63c2
tavovergel <tavovergel@gmail.com>
2025-11-04 08:32:54 -0500

    Conexion desde el front con el back, muestra en pantalla

--- Frontend/InmueblesSub10Front/src/App.jsx
+++ Frontend/InmueblesSub10Front/src/App.jsx
@@ -1,14 +1,29 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import './App.css';
 
 function App() {
-  
+  const [mensaje, setMensaje] = useState("⏳ Conectando con el backend...");
+
+  // Conexion con el back
+  const API_URL = "/api";
+
+  useEffect(() => {
+    fetch(`${API_URL}/`)
+      .then((res) => res.text())
+      .then((data) => setMensaje(data))
+      .catch((err) => {
+        console.error("❌ Error al conectar con el backend:", err);
+        setMensaje("❌ Error al conectar con el backend");
+      });
+  }, [API_URL]); // dependencias
 
   return (
     <>
-       <h1>grupo 10 Integracion Continua</h1>
+      <h1>Grupo 10 - Integración Continua</h1>
+      <h3>{mensaje}</h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
